d26bd7e5113ede8c59e93770f65d568e6ea50239
Heitor Vieira <heitorvieira.pvd@gmail.com>
2024-01-13 17:05:14 -0300

    allowing cors

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	jwtware "github.com/gofiber/jwt/v2"
 	"github.com/heitor582/m3ChatGo/src/configuration"
 	"github.com/heitor582/m3ChatGo/src/routes"
@@ -16,6 +17,12 @@ func init() {
 func main() {
 	var PORT string = ":" + os.Getenv("PORT")
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+        AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+        AllowOrigins:     "*",
+        AllowCredentials: true,
+        AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+    }))
 	routes.SetupAuthRoutes(app)
 	app.Use(jwtware.New(jwtware.Config{
 		SigningKey: []byte(os.Getenv("JWT_SECRET_KEY")),
